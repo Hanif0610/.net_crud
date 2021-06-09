ac7b8d67380854b3ec9e311ac355c79df34f9dbc
Hanif <marbling1293@naver.com>
2021-06-09 23:28:43 +0900

    [ADD] Hello url mapping

---
+++ crud/crud/Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud.Controllers
+{
+    [Route("hello")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+
+        [HttpGet]
+        public string index()
+        {
+            return "Hello, World!";
+        }
+
+        [HttpGet("world/{num}")]
+        public string World(int num)
+        {
+            return "Hello, Wrold!" + num;
+        }
+    }
+}
