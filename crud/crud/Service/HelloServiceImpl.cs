66b5cd59605c81a386857b4a345afdedd132bb4f
Hanif <marbling1293@naver.com>
2021-06-15 15:50:01 +0900

    [ADD] Hello Service

---
+++ crud/crud/Service/HelloServiceImpl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud.Service
+{
+    public class HelloServiceImpl : HelloService
+    {
+        public string getHello() => "Hello, World!";
+    }
+}
