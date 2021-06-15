626f718bbab0e537ce6cac703f9e683dc2ad0b3f
Hanif <marbling1293@naver.com>
2021-06-15 15:49:43 +0900

    [ADD] 뭔지 모를 파일들 업데이트

--- crud/crud/Startup.cs
+++ crud/crud/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using crud.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace crud
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<HelloService, HelloServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
