36e498f08c2442ae7fee44be0d28273c2b9c3a7c
Hanif <marbling1293@naver.com>
2021-06-22 15:15:04 +0900

    [ADD] addUser dto

---
+++ crud/crud/Model/AddUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud.Model
+{
+    public class AddUserDto
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public int height { get; set; }
+    }
+}
