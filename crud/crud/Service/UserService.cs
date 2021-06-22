e851532c4e8134d1815d258489f8e89537116cc6
Hanif <marbling1293@naver.com>
2021-06-22 15:14:43 +0900

    [ADD] addUser service

---
+++ crud/crud/Service/UserService.cs
@@ -0,0 +1,23 @@
+﻿using crud.Entity;
+using crud.Model;
+using System.Collections.Generic;
+
+namespace crud.Service
+{
+    public interface IUserService
+    {
+        int addUser(AddUserDto addUserDto);
+    }
+
+    public class UserService : IUserService
+    {
+        private int cnt = 1;
+        private readonly List<User> users = new List<User> { };
+
+        public int addUser(AddUserDto addUserDto)
+        {
+            users.Add(new User(cnt, addUserDto.name, addUserDto.age, addUserDto.height));
+            return cnt++;
+        }
+    }
+}
