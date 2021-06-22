b0fdbcb80c0469fbc10e5d820efef2687892c299
Hanif <marbling1293@naver.com>
2021-06-22 15:14:51 +0900

    [ADD] addUser Controller

---
+++ crud/crud/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using crud.Model;
+using crud.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud.Controllers
+{
+    [Route("user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("add")]
+        public int AddUser([FromBody]AddUserDto addUserDto) => _userService.addUser(addUserDto);
+    }
+}
