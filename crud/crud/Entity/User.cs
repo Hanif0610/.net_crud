90ef8f0bc60648d7ccd416979680419cd5853735
Hanif <marbling1293@naver.com>
2021-06-22 15:14:57 +0900

    [ADD] user entity

---
+++ crud/crud/Entity/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud.Entity
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public int height { get; set; }
+
+        public User(int _id, string _name, int _age, int _height)
+        {
+            id = _id;
+            name = _name;
+            age = _age;
+            height = _height;
+        }
+    }
+}
