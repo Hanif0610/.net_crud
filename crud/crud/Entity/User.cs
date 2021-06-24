1e118ba8ebdb0622c2d7b659ee8c95cabb3557e8
Hanif <marbling1293@naver.com>
2021-06-24 23:09:39 +0900

    [FIX] 생성자 데이터 대입 this로 변경

--- crud/crud/Entity/User.cs
+++ crud/crud/Entity/User.cs
@@ -7,17 +7,17 @@ namespace crud.Entity
 {
     public class User
     {
-        public int id { get; set; }
-        public string name { get; set; }
-        public int age { get; set; }
-        public int height { get; set; }
+        private int id { get; set; }
+        private string name { get; set; }
+        private int age { get; set; }
+        private int height { get; set; }
 
-        public User(int _id, string _name, int _age, int _height)
+        public User(int id, string name, int age, int height)
         {
-            id = _id;
-            name = _name;
-            age = _age;
-            height = _height;
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.height = height;
         }
     }
 }
