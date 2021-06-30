c7aee14654b24f0055a222d40a162e5241af3070
Hanif <marbling1293@naver.com>
2021-06-30 22:43:04 +0900

    [ADD] Global Exception Handler

---
+++ crud/crud/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace crud.Middleware
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch(error)
+                {
+                    case KeyNotFoundException e:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var errorResponse = new
+                {
+                    message = error.Message,
+                    statusCode = response.StatusCode
+                };
+
+                var errorJson = JsonSerializer.Serialize(errorResponse);
+
+                await response.WriteAsync(errorJson);
+            }
+        }
+    }
+}
