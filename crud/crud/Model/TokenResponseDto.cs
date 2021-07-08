afdf21c37ca2e68058ea14312c80804b698c5a08
Hanif <marbling1293@naver.com>
2021-07-08 09:47:52 +0900

    [ADD] Token response dto

---
+++ crud/crud/Model/TokenResponseDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud.Model
+{
+    public class TokenResponseDto
+    {
+        public string accessToken { get; set; }
+        public string tokenType { get; set; }
+
+        public TokenResponseDto Builder()
+        {
+            return new TokenResponseDto();
+        }
+
+        public TokenResponseDto AccessToken(String accessToken)
+        {
+            this.accessToken = accessToken;
+            return this;
+        }
+
+        public TokenResponseDto TokenType(String tokenType)
+        {
+            this.tokenType = tokenType;
+            return this;
+        }
+
+        public TokenResponseDto Build()
+        {
+            return this;
+        }
+    }
+}
