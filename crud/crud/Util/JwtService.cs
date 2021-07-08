d82a33e011dc5a38583f7978a3bf6011a8dd608c
Hanif <marbling1293@naver.com>
2021-07-08 09:48:18 +0900

    [ADD] JwtService

---
+++ crud/crud/Util/JwtService.cs
@@ -0,0 +1,73 @@
+﻿using crud.Config;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud.Util
+{
+    public interface IJwtService
+    {
+        public string GenerateJwtToken(int accountId);
+        public bool ValidateJwtToken(string token);
+    }
+
+    public class JwtService : IJwtService
+    {
+        private readonly JwtConfig _jwtConfig;
+
+        public JwtService(IOptions<JwtConfig> jwtConfig)
+        {
+            _jwtConfig = jwtConfig.Value;
+        }
+
+        public string GenerateJwtToken(int accountId)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtConfig.secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", accountId.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public bool ValidateJwtToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtConfig.secret);
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var accountId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+
+                // return account id from JWT token if validation successful
+                return true;
+            }
+            catch
+            {
+                // return null if validation fails
+                return false;
+            }
+        }
+    }
+}
